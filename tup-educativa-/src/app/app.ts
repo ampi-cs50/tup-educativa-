4120086495dc7c92b1b4a66e7c7d4bee14ad0633
ampi-cs50 <152340325+ampi-cs50@users.noreply.github.com>
2026-05-06 19:49:30 -0300

    feat: estructura base de Angular pata TP3

--- tup-educativa-/src/app/app.ts
+++ tup-educativa-/src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('mi-proyecto');
+  protected readonly title = signal('tup-educativa-');
 }
